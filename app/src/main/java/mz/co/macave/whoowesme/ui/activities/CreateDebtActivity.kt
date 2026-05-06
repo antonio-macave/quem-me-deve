efbe1115d6d871844407a940663b296f7ec64c3f
António Macave <antonio.macave@gmail.com>
2026-05-06 20:19:39 +0200

    Optimizing imports

--- app/src/main/java/mz/co/macave/whoowesme/ui/activities/CreateDebtActivity.kt
+++ app/src/main/java/mz/co/macave/whoowesme/ui/activities/CreateDebtActivity.kt
@@ -4,19 +4,27 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import mz.co.macave.whoowesme.R
-import mz.co.macave.whoowesme.ui.theme.WhoOwesMeTheme
+import mz.co.macave.whoowesme.data.DatabaseProvider
+import mz.co.macave.whoowesme.data.repository.DebtRepository
+import mz.co.macave.whoowesme.data.repository.DebtorRepository
+import mz.co.macave.whoowesme.model.Debt
 import mz.co.macave.whoowesme.ui.screen.AppBar
 import mz.co.macave.whoowesme.ui.screen.CreateDebt
-
+import mz.co.macave.whoowesme.ui.theme.WhoOwesMeTheme
+import mz.co.macave.whoowesme.util.DebtStatus
+import mz.co.macave.whoowesme.util.toLocalDate
 import mz.co.macave.whoowesme.viewmodel.CreateDebtViewModel
+import mz.co.macave.whoowesme.viewmodel.ViewModelFactory
 
 class CreateDebtActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
