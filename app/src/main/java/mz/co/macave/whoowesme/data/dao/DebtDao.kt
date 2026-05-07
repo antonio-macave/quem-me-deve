463b494b19cdf68d5993484c4785663145c058cc
António Macave <antonio.macave@gmail.com>
2026-05-07 14:23:09 +0200

    refact: getAllDebts returns a Flow now.

--- app/src/main/java/mz/co/macave/whoowesme/data/dao/DebtDao.kt
+++ app/src/main/java/mz/co/macave/whoowesme/data/dao/DebtDao.kt
@@ -4,16 +4,18 @@ import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
 import mz.co.macave.whoowesme.model.Debt
 
 @Dao
 interface DebtDao {
 
     @Query("SELECT * FROM debts")
-    suspend fun getAllDebts(): List<Debt>
+    fun getAllDebts(): Flow<List<Debt>>
 
     @Query("SELECT * FROM debts WHERE id IN (:debtIds)")
     suspend fun loadAllDebtsById(debtIds: IntArray): List<Debt>
+
 
     @Insert
     suspend fun insertAll(vararg debts: Debt)
