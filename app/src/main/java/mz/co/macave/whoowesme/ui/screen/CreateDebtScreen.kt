57ae110477fb80593fba30ac006cd8fcaef13318
António Macave <antonio.macave@gmail.com>
2026-05-07 14:23:47 +0200

    Optimizing imports

--- app/src/main/java/mz/co/macave/whoowesme/ui/screen/CreateDebtScreen.kt
+++ app/src/main/java/mz/co/macave/whoowesme/ui/screen/CreateDebtScreen.kt
@@ -2,7 +2,6 @@ package mz.co.macave.whoowesme.ui.screen
 
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -39,6 +38,8 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.semantics.Role
