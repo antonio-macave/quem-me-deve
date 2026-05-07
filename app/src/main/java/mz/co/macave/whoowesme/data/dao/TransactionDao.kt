785f06ac0411390182ca9447018b833267e01409
António Macave <antonio.macave@gmail.com>
2026-05-07 14:15:52 +0200

    loadAllTransactionsByDebtId returning a Flow of Transaction instead of List of DebtWithTransactions.

--- app/src/main/java/mz/co/macave/whoowesme/data/dao/TransactionDao.kt
+++ app/src/main/java/mz/co/macave/whoowesme/data/dao/TransactionDao.kt
@@ -4,14 +4,15 @@ import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
-import mz.co.macave.whoowesme.model.DebtWithTransactions
+import kotlinx.coroutines.flow.Flow
 import mz.co.macave.whoowesme.model.Transaction
 
 @Dao
 interface TransactionDao {
 
+    @androidx.room.Transaction
     @Query("SELECT * FROM transactions WHERE debtId = :debtId")
-    suspend fun loadAllTransactionsByDebtId(debtId: Int): List<DebtWithTransactions>
+    fun loadAllTransactionsByDebtId(debtId: Int): Flow<List<Transaction>>
 
     @Insert
     suspend fun insertAll(vararg transactions: Transaction)
