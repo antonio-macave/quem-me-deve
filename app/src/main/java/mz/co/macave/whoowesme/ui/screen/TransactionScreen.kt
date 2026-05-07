6e79dda7d714671817d687d3f3c8ac3c7912a3f4
António Macave <antonio.macave@gmail.com>
2026-05-07 14:07:23 +0200

    Optimizing imports

--- app/src/main/java/mz/co/macave/whoowesme/ui/screen/TransactionScreen.kt
+++ app/src/main/java/mz/co/macave/whoowesme/ui/screen/TransactionScreen.kt
@@ -23,15 +23,19 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import mz.co.macave.whoowesme.R
 import mz.co.macave.whoowesme.model.Transaction
-import mz.co.macave.whoowesme.util.formatDate
+import mz.co.macave.whoowesme.util.formatLocalDate
+import mz.co.macave.whoowesme.viewmodel.TransactionsActivityViewModel
 import java.time.LocalDate
 
 @Composable
