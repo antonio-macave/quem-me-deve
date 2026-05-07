987834b19d7018a87773c204c54fe04bd8b700c3
António Macave <antonio.macave@gmail.com>
2026-05-07 11:58:07 +0200

    Optimizing imports

--- app/src/main/java/mz/co/macave/whoowesme/ui/screen/AddTransactionScreen.kt
+++ app/src/main/java/mz/co/macave/whoowesme/ui/screen/AddTransactionScreen.kt
@@ -42,7 +42,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.onFocusEvent
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.semantics.role
 import androidx.compose.ui.semantics.semantics
@@ -54,6 +57,7 @@ import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import mz.co.macave.whoowesme.R
 import mz.co.macave.whoowesme.util.TransactionType
+import mz.co.macave.whoowesme.util.formatDateFromMillis
 import mz.co.macave.whoowesme.viewmodel.AddTransactionViewModel
 
 
