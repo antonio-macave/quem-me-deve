02995340520d70997f6ab07ee378b447ff28a8c3
António Macave <antonio.macave@gmail.com>
2026-05-11 16:31:40 +0200

    Query to retrieve debt and debtor data created.

--- app/src/main/java/mz/co/macave/whoowesme/data/dao/DebtDao.kt
+++ app/src/main/java/mz/co/macave/whoowesme/data/dao/DebtDao.kt
@@ -6,12 +6,30 @@ import androidx.room.Insert
 import androidx.room.Query
 import kotlinx.coroutines.flow.Flow
 import mz.co.macave.whoowesme.model.Debt
+import mz.co.macave.whoowesme.model.DebtCardItem
 
 @Dao
 interface DebtDao {
 
     @Query("SELECT * FROM debts")
     fun getAllDebts(): Flow<List<Debt>>
+
+    @Query("""
+        SELECT 
+            debts.id AS debtId, 
+            debtors.id AS debtorId, 
+            debtors.name AS debtorName,
+            debtors.surname AS debtorSurname,
+            debts.amount AS amount,
+            debts.paidAmount AS paidAmount,
+            debts.status AS status,
+            debts.description AS description,
+            debts.additionalNotes AS additionalNotes,
+            debts.dueTo AS dueTo
+        FROM debts
+        INNER JOIN debtors ON debts.debtorId = debtors.id
+        """)
+    fun findDebtsWithDebtorName(): Flow<List<DebtCardItem>>
 
     @Query("SELECT * FROM debts WHERE id IN (:debtIds)")
     suspend fun loadAllDebtsById(debtIds: IntArray): List<Debt>
--- app/src/main/java/mz/co/macave/whoowesme/data/repository/DebtRepository.kt
+++ app/src/main/java/mz/co/macave/whoowesme/data/repository/DebtRepository.kt
@@ -3,6 +3,7 @@ package mz.co.macave.whoowesme.data.repository
 import kotlinx.coroutines.flow.Flow
 import mz.co.macave.whoowesme.data.dao.DebtDao
 import mz.co.macave.whoowesme.model.Debt
+import mz.co.macave.whoowesme.model.DebtCardItem
 
 class DebtRepository(private val debtDao: DebtDao) {
 
@@ -17,6 +18,11 @@ class DebtRepository(private val debtDao: DebtDao) {
     fun getAllDebts(): Flow<List<Debt>> {
         return debtDao.getAllDebts()
     }
+
+    fun findDebtsWithDebtorName(): Flow<List<DebtCardItem>> {
+        return debtDao.findDebtsWithDebtorName()
+    }
+
 
     suspend fun findDebtsByDebtorId(debtorId: IntArray): List<Debt> {
         return debtDao.loadAllDebtsById(debtorId)
