ace47a02b95d0b3381f135570bc0ea8cf12a176b
António Macave <antonio.macave@gmail.com>
2026-05-16 16:13:28 +0200

    Setting foreignKeys and onDelete action

--- app/src/main/java/mz/co/macave/whoowesme/model/Transaction.kt
+++ app/src/main/java/mz/co/macave/whoowesme/model/Transaction.kt
@@ -1,10 +1,22 @@
 package mz.co.macave.whoowesme.model
 
 import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.ForeignKey.Companion.CASCADE
 import androidx.room.PrimaryKey
 import java.time.LocalDate
 
-@Entity(tableName = "transactions")
+@Entity(
+    tableName = "transactions",
+    foreignKeys = [
+        ForeignKey(
+            entity = Debt::class,
+            parentColumns = ["id"],
+            childColumns = ["debtId"],
+            onDelete = CASCADE
+        )
+    ]
+)
 data class Transaction(
     @PrimaryKey(autoGenerate = true) val id: Int = 0,
     val type: Int,
