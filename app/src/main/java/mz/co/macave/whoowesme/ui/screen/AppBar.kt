3a7e8fff9cb7e354d1f70ff3aba20aa333f69df3
António Macave <antonio.macave@gmail.com>
2026-05-19 18:40:05 +0200

    Replacing CenterTopAlignetTopAppBar by a TopAppBar.

--- app/src/main/java/mz/co/macave/whoowesme/ui/screen/AppBar.kt
+++ app/src/main/java/mz/co/macave/whoowesme/ui/screen/AppBar.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 
@@ -21,7 +22,7 @@ fun AppBar(
     onOkListener: () -> Unit,
     onCancelListener: () -> Unit
 ) {
-    CenterAlignedTopAppBar(
+    TopAppBar(
         title = {
             Text(
                 text = title,
