a995c89351b6409f320320c45f8eb5fb4f4ec543
António Macave <antonio.macave@gmail.com>
2026-05-20 07:58:01 +0200

    ContextMenu Composable created

--- app/src/main/java/mz/co/macave/whoowesme/ui/screen/ContextMenu.kt
+++ app/src/main/java/mz/co/macave/whoowesme/ui/screen/ContextMenu.kt
@@ -1,13 +1,68 @@
 package mz.co.macave.whoowesme.ui.screen
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import mz.co.macave.whoowesme.R
 
+
+@Composable
+fun ContextMenu(
+    menuExpanded: Boolean,
+    onOpenDialogClick: () -> Unit,
+    onDismissRequest: () -> Unit,
+    onEditClick: () -> Unit,
+) {
+    DropdownMenu(
+        expanded = menuExpanded,
+        onDismissRequest = { onDismissRequest() }
+    ) {
+        DropdownMenuItem(
+            modifier = Modifier.padding(vertical = 2.dp),
+            onClick = {
+                onDismissRequest()
+                onEditClick()
+            },
+            text = { Text(text = stringResource(R.string.edit)) },
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Default.Edit,
+                    contentDescription = null,
+                    modifier = Modifier.size(18.dp)
+                )
+            }
+        )
+
+        DropdownMenuItem(
+            modifier = Modifier.padding(vertical = 2.dp),
+            onClick = {
+                onDismissRequest()
+                onOpenDialogClick()
+            },
+            text = { Text(text = stringResource(R.string.delete)) },
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Default.Delete,
+                    contentDescription = null,
+                    modifier = Modifier.size(18.dp)
+                )
+            }
+        )
+    }
+}
 
 @Composable
 fun DeleteConfirmationDialog(
