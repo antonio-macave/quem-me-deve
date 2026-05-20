a23be202cc854237581a5bf8a8f34050779bdd24
António Macave <antonio.macave@gmail.com>
2026-05-20 07:59:45 +0200

    ContextMenuButton Composable created

--- app/src/main/java/mz/co/macave/whoowesme/ui/screen/ContextMenu.kt
+++ app/src/main/java/mz/co/macave/whoowesme/ui/screen/ContextMenu.kt
@@ -1,10 +1,16 @@
 package mz.co.macave.whoowesme.ui.screen
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
@@ -13,10 +19,56 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import mz.co.macave.whoowesme.R
+
+
+@Composable
+fun ContextMenuButton(
+    menuExpanded: Boolean,
+    isDeleteConfirmationDialogOpen: Boolean,
+    onOpenDeleteConfirmationDialog: () -> Unit,
+    onDialogDismissRequest: () -> Unit,
+    onContextDismissRequest: () -> Unit,
+    onButtonClick: () -> Unit,
+    onEditClick: () -> Unit,
+    onDeleteClick: () -> Unit
+) {
+    Column {
+
+        Box(
+            modifier = Modifier
+                .size(32.dp)
+                .clip(RoundedCornerShape(14.dp))
+                .background(MaterialTheme.colorScheme.tertiaryContainer)
+                .clickable { onButtonClick() },
+            contentAlignment = Alignment.Center
+        ) {
+            Icon(
+                imageVector = Icons.Default.MoreVert,
+                contentDescription = null,
+                modifier = Modifier.size(16.dp)
+            )
+        }
+
+        ContextMenu(
+            menuExpanded = menuExpanded,
+            onDismissRequest = { onContextDismissRequest() },
+            onOpenDialogClick = { onOpenDeleteConfirmationDialog() },
+            onEditClick = { onEditClick() },
+        )
+
+        DeleteConfirmationDialog(
+            showDialog = isDeleteConfirmationDialogOpen,
+            onDialogDismissRequest = { onDialogDismissRequest() },
+            onDeleteConfirmation = {  onDeleteClick() }
+        )
+    }
+}
 
 
 @Composable
