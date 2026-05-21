782895c5ee21771a69141123a84eda600eb1f958
António Macave <antonio.macave@gmail.com>
2026-05-21 14:29:29 +0200

    LoadingScreen Composable created in ScreenStates file.

---
+++ app/src/main/java/mz/co/macave/quemmedeve/ui/screen/ScreenStates.kt
@@ -0,0 +1,40 @@
+package mz.co.macave.quemmedeve.ui.screen
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun LoadingScreen() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.background),
+        contentAlignment = Alignment.Center
+    ) {
+
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+            CircularProgressIndicator()
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Text(
+                text = "A carregar...",
+                style = MaterialTheme.typography.bodyMedium
+            )
+        }
+    }
+}
