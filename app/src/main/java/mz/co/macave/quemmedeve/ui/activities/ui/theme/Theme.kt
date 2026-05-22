4b13a06555fffcb058342fb52659c79ac255024f
António Macave <antonio.macave@gmail.com>
2026-05-22 12:47:52 +0200

    android.app.Activity import removed from Theme files

--- app/src/main/java/mz/co/macave/quemmedeve/ui/activities/ui/theme/Theme.kt
+++ app/src/main/java/mz/co/macave/quemmedeve/ui/activities/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package mz.co.macave.quemmedeve.ui.activities.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
--- app/src/main/java/mz/co/macave/quemmedeve/ui/theme/Theme.kt
+++ app/src/main/java/mz/co/macave/quemmedeve/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package mz.co.macave.quemmedeve.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
